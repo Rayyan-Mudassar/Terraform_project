39645e8307f5ec7d2dd235e07b2d4140aa05902c
Rayyan-Mudassar <naziamudassar1985@gmail.com>
2026-04-16 20:04:37 +0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
-variable "instance_type" {
-  default     = "t3.micro"
+variable "region" {
+    default = "us-east-1"
 }
 
-variable "region" {
-  default     = "us-east-1"
+variable "instance_type" {
+    default = "t3.micro"
 }
